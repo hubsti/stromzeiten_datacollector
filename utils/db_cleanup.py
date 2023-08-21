47a406bc4a57e115b8914c0fb039613ace60f43d
hubsti <hubsti@wp.pl>
2023-08-21 20:23:43 +0200

    remove table name from the query

--- utils/db_cleanup.py
+++ utils/db_cleanup.py
@@ -12,7 +12,7 @@ def remove_dupilcates(df, dbengine, country_code, table):
     #print(dataFrame)
     with psycopg.connect("dbname=stromzeiten user=admin host=localhost password=admin") as conn:
         with conn.cursor() as cur:
-                cur.execute(f"DELETE FROM {table} WHERE index>=\'{start_date}\' and index <=\'{end_date}\' and generation_acc.country_code = \'{country_code}\' ;") 
+                cur.execute(f"DELETE FROM {table} WHERE index>=\'{start_date}\' and index <=\'{end_date}\' and country_code = \'{country_code}\' ;") 
                 conn.commit()    
                 conn.close()
     #dataFrame       = pd.read_sql(f"select * from \"{table}\"", dbengine)
